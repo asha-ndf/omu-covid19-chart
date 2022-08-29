d51c129e3728d84fb7b215b1b3d350e3cac51cfc
asha-ndf <ashanomail@gmail.com>
2022-08-30 03:09:12 +0900

    ver1.1

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# omu-covid19-chart(1.0)
+# omu-covid19-chart(1.1)
 ## サイト
 * [https://asha-ndf.github.io/omu-covid19-chart/](https://asha-ndf.github.io/omu-covid19-chart/)
 ## 概要
 * 大阪公立大学の新型コロナウイルス感染症の発生状況(検査結果判明日)をグラフ化したものです。</p>
-* データは2022年8月5日時点のものを使用しています。</p>
-* 最新の情報は[大学ホームページ](https://www.omu.ac.jp/about/efforts/covid19/)をご覧ください。</p>
+* データは大阪公立大学が公開しているPDFより、毎日定時に自動取得しています。</p>
+* 最新の情報・PDFは[大学ホームページ](https://www.omu.ac.jp/about/efforts/covid19/)をご覧ください。</p>
 ## 注意
 * データ処理にあたり、検査結果判明日が不明とされているものは除外してあります。
 ## Github Pages ステータス
--- getdata.py
+++ getdata.py
@@ -1,11 +1,14 @@
-print("=========開始=========")
-import pandas as pd
-import tabula
-import re
-import requests
 import datetime
+import requests
+import re
+import tabula
+import pandas as pd
+print("=========開始=========")
+# import
 
-load_url = "https://www.omu.ac.jp/about/efforts/covid19/"
+# ==========================================================
+# PDFのリンクを取得する
+load_url = "https://www.omu.ac.jp/about/efforts/covid19/"  # PDFのリンクが掲載されるWebページ
 html = requests.get(load_url)
 
 
@@ -15,38 +18,57 @@ pdf_path = "https://www.omu.ac.jp/assets/" + urls[0]
 print("データ取得元URL")
 print(urls)
 
-# lattice=Trueでテーブルの軸線でセルを判定
-dfs = tabula.read_pdf(pdf_path, lattice=True, pandas_options={'header': None}, pages = 'all')
+# ==========================================================
+# PDFのからデータを取得し、処理をする
+# テーブルの軸線でセルを判定してデータを取得
+dfs = tabula.read_pdf(pdf_path, lattice=True, pandas_options={
+                      'header': None}, pages='all')
 
 use_df = dfs[2].drop(0, axis=0)
 for df in dfs[3:]:
-    use_df = pd.concat([use_df,df])
+    use_df = pd.concat([use_df, df])
 use_df = use_df[use_df[0] != "不明"]
 use_df.reset_index(drop=True)
 
-def to_datetime(i):
-  return datetime.datetime.strptime("2022/"+str(i).split("(")[0], '%Y/%m/%d')
-use_df[0] = use_df[0].apply(to_datetime)
-def to_timestamp(i):
-  return i.timestamp()
-use_df[6] = use_df[0].apply(to_timestamp)
-def to_number(i):
-  return int(i[:len(i)-1])
-use_df[2] = use_df[2].apply(to_number)
-use_df[3] = use_df[3].replace("阿部野","阿倍野")
 
+def to_datetime(i):
+    return datetime.datetime.strptime("2022/"+str(i).split("(")[0], '%Y/%m/%d')
+
+
+use_df[0] = use_df[0].apply(to_datetime)
+
+
+def to_timestamp(i):
+    return i.timestamp()
+
+
+use_df[6] = use_df[0].apply(to_timestamp)
+
+
+def to_number(i):
+    return int(i[:len(i)-1])
+
+
+use_df[2] = use_df[2].apply(to_number)
+use_df[3] = use_df[3].replace("阿部野", "阿倍野")  # 文字の誤りを修正
+
+# 旧府大
 gb_opu = use_df[use_df[3] == '中百舌鳥・羽曳野・りんくう'].groupby(0)
 opu = pd.DataFrame(gb_opu[2].sum()).rename(columns={2: 'opu'})
+# 旧市大
 gb_ocu = use_df[use_df[3] != '中百舌鳥・羽曳野・りんくう'].groupby(0)
 ocu = pd.DataFrame(gb_ocu[2].sum()).rename(columns={2: 'ocu'})
-
+# 公立大
 gb_omu = use_df.groupby(0)
 omu = pd.DataFrame(gb_omu[2].sum()).rename(columns={2: 'omu'})
 
-all = pd.concat([omu,opu,ocu], axis=1).fillna(0)
+# 統合
+all = pd.concat([omu, opu, ocu], axis=1).fillna(0)
 all["opu"] = all["opu"].astype('int')
 all["ocu"] = all["ocu"].astype('int')
 print(all)
+
+# CSVファイルへ書き出し
 all.to_csv("data/graph_data.csv")
 print("SUCCESS")
 print("=========終了=========")
